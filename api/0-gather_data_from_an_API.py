c36c06bd8cd688856ed934b8083462507f07650c
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-15 16:31:01 -0400

    Refactor API data gathering script and improve task summary printing

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,49 +1,19 @@
 #!/usr/bin/python3
 """
-This module uses Python to make requests to a REST API.
-fix
-It fetches data about a specific employee's tasks
-and prints a summary of the tasks completed and the
-titles of the completed tasks.
+Given a specific API, returns information about the user's TODO list progress
 """
 import requests
-import sys
+from sys import argv
 
-
-# Get the employee ID from the command line arguments
-employee_id = sys.argv[1]
-
-# Send a GET request to the API to get the user data
-user_response = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + employee_id)
-
-# Parse the response data as JSON
-data = user_response.json()
-
-# Extract the employee's name from the data
-employee_name = data['name']
-
-# Send another GET request to the API to get the todo data
-todos_response = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
-
-# Parse the todo data as JSON
-todos_data = todos_response.json()
-
-# Calculate the total number of tasks
-total_todos = str(len(todos_data))
-
-# Calculate the number of completed tasks
-completed_todos = str(sum(1 for task in todos_data if task['completed']))
-
-# Print the first line of the output
-print("Employee " + employee_name + " is done with tasks(" +
-      completed_todos + "/" + total_todos + "):")
-
-# Print the title of each completed task
-for task in todos_data:
-    if task['completed']:
-        print('\t ' + task['title'])
-
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    response = requests.get(url)
+    user = response.json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        argv[1])
+    response = requests.get(url)
+    todos = response.json()
+    completed = [todo for todo in todos if todo.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed), len(todos)))
+    [print("\t {}".format(todo.get("title"))) for todo in completed]
