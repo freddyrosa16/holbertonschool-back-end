92f0cedef1efa9fa710905d078e5e9772cfed3ef
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-16 22:12:39 -0400

    Refactor API data gathering script and improve task summary printing

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""
-Using a REST API, for a given employee ID, returns information about his/her TODO list progress.
-"""
+"""information about his/her todo list progress"""
 import requests
 import sys
 
-if __name__ == "__main__":
 
-    employee_id = sys.argv[1]
-
+def gather_data_from_API(user_id):
+    """
+        Retrieves user information and tasks
+        from an API based on the given user ID.
+    """
     response = requests.get(
-        "https://jsonplaceholder.typicode.com/users" + employee_id)
-    data = response.json()
-    employee_name = data['name']
-    todo_request = requests.get(
-        'https://jsonplaceholder.typicode.com/posts?userId=1' + employee_id)
-    todos_data = response.json()
-    total_todo = str(len(todos_data))
-    completed_todos = str(sum(1 for task in todos_data if task['completed']))
-    print("Employee " + employee_name + " is done with tasks(" +
-          completed_todos + "/" + total_todo + "):")
-    for task in todos_data:
-        if task['completed']:
-            print('\t ' + task['title'])
+        f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    if response.status_code == 200:
+        user_name = response.json()["name"]
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
+    if response.status_code == 200:
+        tasks = response.json()
+        number_tasks = len(tasks)
+    completed_tasks = []
+    for task in tasks:
+        if task["completed"]:
+            completed_tasks.append(task)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, len(completed_tasks), number_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task["title"]))
+
 
 if __name__ == '__main__':
-    pass
+    """call the funcion"""
+    gather_data_from_API(int(sys.argv[1]))
